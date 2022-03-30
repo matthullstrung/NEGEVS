55502ded0ab453996e7514e2117d0a506593bfab
matthullstrung <matthullstrung@gmail.com>
2022-03-29 23:56:47 -0400

    Added sample environment with functional portals

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,27 @@
+﻿//With thanks to Sebastian Lague, https://github.com/SebLague
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+    Portal[] portals;
+
+    void Awake () {
+        portals = FindObjectsOfType<Portal> ();
+    }
+
+    void OnPreCull () {
+
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].PrePortalRender ();
+        }
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].Render ();
+        }
+
+        for (int i = 0; i < portals.Length; i++) {
+            portals[i].PostPortalRender ();
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/MoveObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : PortalTraveller {
+    public float maxSpeed = 1;
+    float speed;
+    float targetSpeed;
+    float smoothV;
+    bool forward = true;
+
+    void Start () {
+        Debug.Log ("Press C to stop/start car");
+        targetSpeed = maxSpeed;
+    }
+
+    void Update () {
+        float moveDst = Time.deltaTime * speed;
+
+        if(forward)
+            transform.position += transform.forward * Time.deltaTime * speed;
+        else
+            transform.position -= transform.forward * Time.deltaTime * speed;
+
+        if (Input.GetKeyDown (KeyCode.C)) {
+            targetSpeed = (targetSpeed == 0) ? maxSpeed : 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            forward = !forward;
+        }
+
+        speed = Mathf.SmoothDamp (speed, targetSpeed, ref smoothV, .5f);
+    }
+}
